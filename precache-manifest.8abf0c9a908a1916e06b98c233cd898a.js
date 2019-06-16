e176bb1e7e288973d09140e81fb7fff1530dcf7a
neilchou <neilchou2013@gmail.com>
2019-06-16 11:39:14 +0800

    Updates

---
+++ precache-manifest.8abf0c9a908a1916e06b98c233cd898a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ea8486439bbe9fea9fea2f84d11555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c60baaee9d37ae19d76",
+    "url": "/static/css/main.85b607a5.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9c60baaee9d37ae19d76",
+    "url": "/static/js/main.2817daf7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
