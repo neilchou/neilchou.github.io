45494796f6276f1982fdc5f9d17e214b388bffb5
neilchou <neilchou2013@gmail.com>
2019-07-08 18:39:48 +0800

    组件分离

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,35 +1,21 @@
-import React from 'react'
+import React from 'react';
+import TemperatureInput from './TemperatureInput'
 
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
-}
+// function BoilingVerdict(props) {
+//   if (props.celsius >= 100) {
+//     return <p>The water would boil.</p>;
+//   }
+//   return <p>The water would not boil.</p>;
+// }
 
 export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { temperature: '' };
-  }
-
-  handleChange = (e) => {
-    this.setState({ temperature: e.target.value });
-  }
 
   render() {
-    const temperature = this.state.temperature;
     return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input
-          value={temperature}
-          onChange={this.handleChange} />
-
-        <BoilingVerdict
-          celsius={parseFloat(temperature)} />
-
-      </fieldset>
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
     );
   }
 }
