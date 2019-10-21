be7741f6a7914643e39a1dfb606465a13bf6d801
neilchou <neilchou2013@gmail.com>
2019-10-22 00:32:33 +0800

    Updates

---
+++ precache-manifest.3993febcafa6c81604c1dcde0dcaec8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea95deceeaa5406f13fd467772d0962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2724985994733bacc2d3",
+    "url": "/static/css/main.6f9ba3c6.chunk.css"
+  },
+  {
+    "revision": "4f63cbe7acd979f15c61",
+    "url": "/static/js/2.161843f6.chunk.js"
+  },
+  {
+    "revision": "2724985994733bacc2d3",
+    "url": "/static/js/main.a2b08174.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de7867bc8ceb3dcb187ebb86a5b97fdf.js"
+  "/precache-manifest.3993febcafa6c81604c1dcde0dcaec8b.js"
 );
 
 self.addEventListener('message', (event) => {
